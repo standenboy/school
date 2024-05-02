0ca35b27de52a5d3acf5f2eb877a440c1103e928
standenboy <standenboy@seacrossedlovers.xyz>
2024-05-02 12:14:32 +0100

    did a load of work to make everything better and progress the writeup

--- comp/lucas-standen-NEA/writeup/make.sh
+++ comp/lucas-standen-NEA/writeup/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-for i in $(ls | grep .ms); do groff -ms $i -T pdf > $(echo $i | cut -f1 -d".").pdf;  done
-zathura coverpage.pdf
+for i in $(ls | grep .ms); do groff -ms $i -T ps > $(echo $i | cut -f1 -d".").ps;  ps2pdf $(echo $i | cut -f1 -d".").ps; done
+zathura coverpage.pdf &
