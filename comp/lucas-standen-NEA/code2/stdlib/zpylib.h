997fd49c81a86696db6f62ce8c5bdde038c7cebb
thing1 <thing1@seacrossedlovers.xyz>
2024-09-24 11:30:01 +0100

    removed the guest book because it wasnt working

--- comp/lucas-standen-NEA/code2/stdlib/zpylib.h
+++ comp/lucas-standen-NEA/code2/stdlib/zpylib.h
@@ -1,7 +1,9 @@
 #include <String.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 void printstr(char *str);
+void printchar(char c);
 void printint(int i);
 void printfloat(double f);
 char *readstr();
